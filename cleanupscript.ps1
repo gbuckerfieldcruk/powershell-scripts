6341ef87276718ce556a25558a12f4227d2122ac
georgebuckerfield <georgebuckerfield@gmail.com>
2014-06-11 17:04:11 +0100

    Additional comments, changed variables

--- cleanupscript.ps1
+++ cleanupscript.ps1
@@ -1,10 +1,15 @@
-#$limit = (get-date).adddays(-15)
+# Delete all files in a given path that are older than a specified number of days
+#
+# 
+
+$limit = 365
 $path = "\\someserver\someshare"
 
 $files = ls $path -recurse
 foreach($x in $files)
     {$y = ((get-date) - $x.creationtime).days
-    if ($y -gt 365 -and $x.psiscontainer -ne $true)
+    if ($y -gt $limit -and $x.psiscontainer -ne $true)
+        # To delete files, replace this line with {$x.delete()}
         {write-host $x.name}
     }
  
